9a0fc3e337c67cb109a1374e4ece3d1f309cc5e7
Tommaso De Vecchi <t.devecchi@donboscosandona.it>
2023-03-30 11:56:07 +0200

    Creato il componente footer vuoto

---
+++ dev/tmomo/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer(props) {
+    return (
+        <h1>jhasfndnrfgeresrgre</h1>
+    )
+}
---
+++ pages/dev/tmomo/index.js
@@ -0,0 +1,11 @@
+import Layout from 'components/Layout'
+import Footer from 'dev/tmomo/Footer'
+
+export default function Page() {
+    return (
+        <Layout>
+            <h1>Ciao</h1>
+            <Footer/>
+        </Layout>
+    )
+}
